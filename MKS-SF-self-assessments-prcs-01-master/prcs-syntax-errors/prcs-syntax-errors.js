f8fdfec0182d64e5f60e4c7a676e44c7b222e54d
Eric Lau <elau002@gmail.com>
2016-10-12 11:04:18 -0700

    Find the 1 in the syntaxalyze.html when there are no errors funny.

---
+++ MKS-SF-self-assessments-prcs-01-master/prcs-syntax-errors/prcs-syntax-errors.js
@@ -0,0 +1,12 @@
+var sum = function(arr) {
+  var result = 0;
+  var i;
+  for (i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === "number") {
+      result += arr[i]
+    }
+  }
+  return result;
+};
+
+
